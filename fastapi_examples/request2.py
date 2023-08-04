b24b5c24b46e68c0bcf41ca69a9fffa37b956875
pranavmenon2408 <pranav2408dhruv@gmail.com>
2023-08-04 22:54:48 +0530

    commit

--- fastapi_examples/request2.py
+++ fastapi_examples/request2.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Path
+from fastapi import FastAPI,Path,Body
 from typing import Annotated
 from pydantic import BaseModel
 
@@ -9,11 +9,17 @@ class Item(BaseModel):
     price:float
     tax:float |None=None
 
+class User(BaseModel):
+    username:str
+    fullname:str |None=None
+
 @app.put("/items/{item_id}")
-async def root(item_id:Annotated[int, Path(title="Id of item",ge=0,le=1000)],q:str |None=None,item:Item |None=None):
+async def root(item_id:Annotated[int, Path(title="Id of item",ge=0,le=1000)],q:str |None=None,item:Item |None=None,user: User |None=None):
     results={"item_id":item_id}
     if q:
         results.update({"q":q})
     if item:
         results.update({"item":item})
+    if user:
+        results.update({"user":user})
     return results
